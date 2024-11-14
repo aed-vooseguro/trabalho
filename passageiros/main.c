93c978aafa803036faac70b6d32f7fe6057dabf0
Caio Ribeiro Tavares <caiotavares102@gmail.com>
2024-11-14 10:35:21 -0300

    struct dados passageiro

---
+++ passageiros/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int codigo;
+    char nome[50];
+    char endereco[100];
+    char telefone[15];
+    int fidelidade; // 1 para sim, 0 para não
+    int pontosFidelidade;
+} Passageiro;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
