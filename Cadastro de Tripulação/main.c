e113e0db17cedbeb7626374cb75ab1fe921201ca
Vinicius Michetti Carvalho <viniciusmichetti2@gmail.com>
2024-11-18 12:53:39 -0300

    tripulação 1.1
    Atualização Cadastro Tripulação

--- Cadastro de Tripulação/main.c
+++ Cadastro de Tripulação/main.c
@@ -10,6 +10,38 @@ typedef struct{
 
 void cadastro_tripulacao(int codigo, char nome, char telefone, char cargo){
 
+     tripulacao t[50];
+     int i = 0;
+
+     do{
+
+        printf("Digite o codigo da pessoa %d: ", i + 1);
+        scanf("%d", &t[i].codigo);
+        getchar();
+
+        if (t[i].codigo < 0){
+            printf("Codigos negativos sao invalidos. Digite novamente.\n");
+            continue;
+        }
+
+        printf("Digite o nome da pessoa %d: ", i + 1);
+        fgets(t[i].nome, 20, stdin);
+        fflush(stdin);
+
+        printf("Digite o telefone da pessoa %d: ", i + 1);
+        fgets(t[i].telefone, 20, stdin);
+        fflush(stdin);
+
+        printf("Digite o cargo da pessoa %d: ", i + 1);
+        fgets(t[i].telefone, 20, stdin);
+        fflush(stdin);
+
+
+        i++;
+
+     }while(1);
+
+
 }
 
 int main()
