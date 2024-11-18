7b5a5717acf539cec9a325b6a82e8b718c42dd00
Vinicius Michetti Carvalho <viniciusmichetti2@gmail.com>
2024-11-18 15:24:57 -0300

    Tripulação 1.4
    Atualização do Cadastro de Tripulação

--- Cadastro de Tripulação/main.c
+++ Cadastro de Tripulação/main.c
@@ -14,8 +14,11 @@ void cadastro_tripulacao(int codigo, char nome, char telefone, char cargo){
      tripulacao t[50];
      int codigos[50];
      int i = 0, j = 0;
+     char a[6];
 
      do{
+
+        printf("Cadastro da Tripulacao: \n");
 
         printf("Digite o codigo da pessoa %d: ", i + 1);
         scanf("%d", &t[i].codigo);
@@ -33,15 +36,34 @@ void cadastro_tripulacao(int codigo, char nome, char telefone, char cargo){
 
         printf("Digite o nome da pessoa %d: ", i + 1);
         fgets(t[i].nome, 20, stdin);
-        fflush(stdin);
+
+        t[i].nome[strcspn(t[i].nome, "\n")] = '\0';
+
+        if (strcmp(t[i].nome, "SAIR") == 0){
+            printf("Voce saiu do sistema.");
+            break;
+        }
+
 
         printf("Digite o telefone da pessoa %d: ", i + 1);
         fgets(t[i].telefone, 20, stdin);
-        fflush(stdin);
+
+        t[i].telefone[strcspn(t[i].telefone, "\n")] = '\0';
+
+        if (strcmp(t[i].telefone, "SAIR") == 0){
+            printf("Voce saiu do sistema.");
+            break;
+        }
 
         printf("Digite o cargo da pessoa %d: ", i + 1);
-        fgets(t[i].telefone, 20, stdin);
-        fflush(stdin);
+        fgets(t[i].cargo, 10, stdin);
+
+        t[i].cargo[strcspn(t[i].cargo, "\n")] = '\0';
+
+        if (strcmp(t[i].cargo, "SAIR") == 0){
+            printf("Voce saiu do sistema.");
+            break;
+        }
 
         codigos[j] = t[i].codigo;
         j++;
