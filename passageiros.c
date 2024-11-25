38662cd0fc7a072f7bfbba474ed43e76f8842f46
Mateus Ferrão da Costa <mateusferraocosta2000@gmail.com>
2024-11-25 19:56:50 -0300

    structs de passageiros.c passadas para o header

--- passageiros.c
+++ passageiros.c
@@ -4,20 +4,6 @@
 #include "header.h"
 
 
-// typedef struct {
-//     int codigo;
-//     char nome[50];
-//     char endereco[100];
-//     char telefone[15];
-//     int fidelidade; // 1 para sim, 0 para n�o
-//     int pontosFidelidade;
-// } Passageiro;
-
-// typedef struct {
-//     char username[20];
-//     char senha[20];
-// } Administrador;
-
 void cadastrarPassageiro(Passageiro *passageiro, int *codigoAtual) {
     passageiro->codigo = (*codigoAtual)++; // Incrementa o c�digo automaticamente
 
